166c0c928365bfdf3e31895c7a0c460b9447c33e
kjune922 <kjune922@naver.com>
2026-09-12 14:08:54 +0900

    feat: ãProgressingStatus - 처리중, 처리 완료, 대기 로 보이게 수정

--- src/main/java/com/kjune922/waterapi/inspection/ProcessingStatus.java
+++ src/main/java/com/kjune922/waterapi/inspection/ProcessingStatus.java
@@ -1,5 +1,15 @@
 package com.kjune922.waterapi.inspection;
 
 public enum ProcessingStatus {
-    PENDING, IN_PROGRESS, COMPLETED
+    PENDING("대기"), IN_PROGRESS("처리 중"), COMPLETED("처리 완료");
+
+    private final String description;
+
+    ProcessingStatus(String description){
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
